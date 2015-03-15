132f15c394d40fa3934170475d41d49255fac644
Valérian Galliat <val@codejam.info>
2015-03-15 10:15:17 +0100

    Add a '--save' option
    It will save migrated repositories to the 'ignore' key of the
    configuration file, so they are not migrated for the next runs.

--- src/cli.js
+++ src/cli.js
@@ -5,7 +5,10 @@ const chalk = require('chalk')
 const { docopt } = require('docopt')
 const read = denodeify(require('read'))
 const readFile = denodeify(require('fs').readFile)
-const yaml = require('js-yaml').safeLoad
+const writeFile = denodeify(require('fs').writeFile)
+const yaml = require('js-yaml')
+const yamlUpdate = require('yaml-update').edit
+const zipObject = require('lodash.zipobject')
 
 const migrate = require('./')
 const github = require('./github')
@@ -27,7 +30,13 @@ Options:
   --mirror              Create mirror repositories.
   --private             Force private repositories (copy source visibility otherwise).
   --with-forks          Include forked repositories.
+  --save                Save migrated repositories to configuration file to avoid
+                        downloading them again the next time.
 `.trim()
+
+// Edit YAML in place (wrapper to avoid passing JSON).
+const yamlEdit = (source, data) =>
+  yamlUpdate(source, JSON.stringify(data))
 
 // Set properties on given object from a stream of name/value pairs.
 const setProperties = object =>
@@ -40,7 +49,14 @@ const setProperties = object =>
 const config = opts => !opts['--config']
   ? [opts]
   : readFile(opts['--config'], 'utf8')
-      .map(yaml)
+      // Keep original source and object for further usage.
+      .map(source => [source, yaml.safeLoad(source)])
+      .map(([source, data]) => Object.assign({}, data, {
+        'config-source': source,
+        'config-object': data,
+      }))
+
+      // Convert to options and extend `opts`.
       .flatMap(_.pairs)
       .map(([name, value]) => [`--${name}`, value])
       .pipe(setProperties(opts))
@@ -75,6 +91,11 @@ const opts = argv =>
     .flatMap(config)
     .flatMap(prompt)
 
+    // Make sure `--ignore` is a hash.
+    .map(opts => Object.assign({}, opts, {
+      '--ignore': zipObject(opts['--ignore']),
+    }))
+
 // Get repository stream from configured sources.
 const repos = opts =>
   _([
@@ -91,11 +112,60 @@ const repos = opts =>
     .compact() // Remove falsy values.
     .sequence() // Merge inner streams.
 
-// From fulfilled options, get repositories and migrate.
-const main = opts =>
-  repos(opts)
+    // Apply filters.
     .filter(opts['--with-forks'] ? repo => true : repo => !repo.fork)
+    .filter(repo => !(repo.name in opts['--ignore']))
 
+// UI for regular migration stream.
+const migrateStream = repoStream =>
+  repoStream.fork()
+    .map(({ repo }) => `${chalk.green('>>')} ${repo.name}`)
+
+    .errors((e, push) => {
+      if (e.repo) push(undefined, chalk.red(`>> ${e.repo.name} `) + e)
+      else push(e)
+    })
+
+const saveConfig = (opts, repos) =>
+  [[opts['--config'], yamlEdit(
+    opts['--config-source'],
+    Object.assign(opts['--config-object'], {
+      ignore: (Object.keys(opts['--ignore'] || {})).concat(repos),
+    })
+  )]]
+
+const saveNew = repos =>
+  _(read({
+    prompt: ask('Where to save migrated repos (file will be overwritten)?'),
+  }))
+    .map(file => [file, yaml.safeDump({ ignore: repos })])
+
+// Save migrated repositories in configuration file at the end.
+const saveStream = (opts, repoStream) =>
+  repoStream.fork()
+    .map(({ repo }) => repo.name)
+    .reduce([], (memo, repo) => memo.concat([repo]))
+
+    // Do nothing if there was no repositories.
+    .filter(repos => repos.length)
+
+    // Append migrated repositories to ignore list.
+    .flatMap(repos => opts['--config']
+      ? saveConfig(opts, repos)
+      : saveNew(repos)
+    )
+
+    .flatMap(([file, yaml]) => writeFile(file, yaml))
+    .map(() => `${chalk.blue('>>')} Configuration written!`)
+
+    // Ignore repository errors (already handled).
+    .errors((e, push) => {
+      if (!e.repo) push(e)
+    })
+
+// From fulfilled options, get repositories and migrate.
+const main = opts => {
+  const repoStream = repos(opts)
     .flatMap(migrate({
       prefix: opts['--gogs'],
       user: opts['--gogs-user'],
@@ -105,12 +175,12 @@ const main = opts =>
       private: opts['--private'],
     }))
 
-    .map(({ repo }) => `${chalk.green('>>')} ${repo.name}`)
+  const stream = migrateStream(repoStream)
 
-    .errors((e, push) => {
-      if (e.repo) push(undefined, chalk.red(`>> ${e.repo.name} `) + e)
-      else push(e)
-    })
+  return opts['--save']
+    ? _([stream, saveStream(opts, repoStream)]).merge()
+    : stream
+}
 
 export default argv =>
   opts(argv)
